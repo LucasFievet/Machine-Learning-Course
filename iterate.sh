9ce26fde8a3c3ce005c9068f6b23876c8d251cec
Kevin Kleber <kkleber@ethz.ch>
2016-10-19 15:24:24 +0200

    ...

--- iterate.sh
+++ iterate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-bsub -J job_chain -B -N -R "rusage[mem=5000]" "python3 Machine-Learning-Course/main.py predict_cut_iterate"
-for i in `seq 2 21`;
+for i in `seq 0 20`;
 do
-	bsub -J job_chain -w "done(job_chain)" -B -N -R "rusage[mem=5000]" "python3 Machine-Learning-Course/main.py predict_cut_iterate"
+	bsub -J job$i -B -N -R "rusage[mem=5000]" "python3 Machine-Learning-Course/main.py predict_cut_iterate $i"
 done  
