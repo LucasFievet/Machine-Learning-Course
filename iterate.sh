75b67a1277ff38049ab04b1bf1f083717da80a0f
Kevin Kleber <kkleber@ethz.ch>
2016-10-19 17:27:48 +0200

    ...

--- iterate.sh
+++ iterate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in `seq 0 20`;
 do
-	bsub -J job$i -B -N -R "rusage[mem=5000]" "python3 main.py predict_cut_iterate $i"
+	bsub -J job$i -B -N -R "rusage[mem=1200]" "python3 main.py predict_cut_iterate $i"
 done  
