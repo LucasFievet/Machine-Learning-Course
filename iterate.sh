304614573846a3ce15b38be3fd265af8ce96536c
Kevin Kleber <kkleber@ethz.ch>
2016-10-20 00:47:52 +0200

    ...

--- iterate.sh
+++ iterate.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 #steps = 10000
 #feature_iterations = 2096920
-#iterations = 53
-#threads = 48
 
 memory=1000
 
-for i in `seq 0 4`;
+for i in `seq 0 210`;
 do
-	for j in `seq 0 47`;
-	do
-		num=$(expr $i \* 48 + $j)
-		bsub -J job_chain_$num -n 1 -R "rusage[mem=$memory]" "python3 main.py predict_cut_iterate $num"
-		echo $num
-	done
+		bsub -J job_chain_$i -n 1 -R "rusage[mem=$memory]" "python3 main.py predict_cut_iterate $i"
+		echo $i
 done  
  
