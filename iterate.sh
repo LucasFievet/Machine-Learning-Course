637c62a0a30884ac1c303798f3d1471698406e66
Kevin Kleber <kkleber@ethz.ch>
2016-10-19 20:26:24 +0200

    ...

--- iterate.sh
+++ iterate.sh
@@ -9,14 +9,16 @@ memory=1000
 for i in `seq 0 52`;
 do
 	for j in `seq 0 39`;
-	do		
+	do
 		num=$(expr $i \* 40 + $j)
 		if [ $i -eq 0 ]
 		then
-			bsub -J job_chain_$j -n 1 -R "rusage[mem=$memory]" "python3 main.py predict_cut_iterate $num"
+			bsub -J job_chain_$num -n 1 -R "rusage[mem=$memory]" "python3 main.py predict_cut_iterate $num"
 		else
-			bsub -J job_chain_$j -w "done(job_chain_$j)" -R -n 1 "rusage[mem=$memory]" "python3 main.py predict_cut_iterate $num"
+			numlj=$(expr ($i - 1) * 40 + $j)
+			bsub -J job_chain_$num -w "done(job_chain_$numlj)" -R -n 1 "rusage[mem=$memory]" "python3 main.py predict_cut_iterate $num"
 		fi
+		echo $num
 	done
 done  
  
