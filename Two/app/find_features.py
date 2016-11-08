8908619a4716dfccce235be1044649010fad8ddc
Kevin Kleber <kkleber@ethz.ch>
2016-11-08 19:36:44 +0100

    ...

--- Two/app/find_features.py
+++ Two/app/find_features.py
@@ -20,16 +20,14 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 class FindFeatures():
     " TODO doc "
     def __init__(self, bin_size=200, box_size=20, thresh=250):
-        self.__var_fac = 2
         data = ReduceHistogram(bin_size, box_size)
-        self.__data = np.transpose(data.get_reduced_set('train'), (2, 1, 0))
-        self.__train = data.get_reduced_set('train')
-        self.__test = data.get_reduced_set('test')
-        self.__targets = load_targets()['Y'].tolist()
-        self.__evaluated = np.array([])
-        self.__evaluate_features()
-        self.__thresh = thresh
-        self.__locations = self.__exctract_significant()
+        self.__vars = {'fac': 2, 'thresh': thresh}
+        self.__vars['data'] = np.transpose(data.get_reduced_set('train'), (2, 1, 0))
+        self.__vars['train'] = data.get_reduced_set('train')
+        self.__vars['test'] = data.get_reduced_set('test')
+        self.__vars['targets'] = load_targets()['Y'].tolist()
+        self.__vars['evaluated'] = self.__evaluate_features()
+        self.__vars['locations'] = self.__exctract_significant()
 
     def get_significant(self, typ='train'):
         """ doc
@@ -37,21 +35,21 @@ class FindFeatures():
         return np.array(self.__exctract_values(typ))
 
     def __exctract_values(self, typ='train'):
-        data = self.__train if typ is 'train' else self.__test
-        loc = self.__locations
+        data = self.__vars[typ]
+        loc = self.__vars['locations']
         range_d = range(np.shape(data)[0])
         range_l = range(np.shape(loc)[0])
         return [[data[d, loc[l, 1], loc[l, 0]] for l in range_l] for d in range_d]
 
 
     def __exctract_significant(self):
-        evaluated = self.__evaluated
+        evaluated = self.__vars['evaluated']
         shape = evaluated[:, :, 0].shape
         mean_dif = np.fabs(evaluated[:, :, 0]-evaluated[:, :, 2])
-        var_avg = np.sqrt(np.divide((evaluated[:, :, 1]+evaluated[:, :, 3]), self.__var_fac))
+        var_avg = np.sqrt(np.divide((evaluated[:, :, 1]+evaluated[:, :, 3]), self.__vars['fac']))
         iteration = [range(shape[0]), range(shape[1])]
         locations = [c for c in itertools.product(*iteration)
-                     if mean_dif[c] > self.__thresh
+                     if mean_dif[c] > self.__vars['thresh']
                      if mean_dif[c] > var_avg[c]]
         print('Number of significant areas found:', np.shape(locations)[0])
         return np.array(locations)
@@ -59,19 +57,20 @@ class FindFeatures():
     def __evaluate_features(self):
         file_path = os.path.join(CACHE_DIRECTORY, 'evaluate_features.mat')
         if os.path.exists(file_path):
-            self.__evaluated = scipy.io.loadmat(file_path)['data']
+            out = scipy.io.loadmat(file_path)['data']
         else:
-            shape = self.__data[:, :, 0].shape
+            shape = self.__vars['data'][:, :, 0].shape
             intervals = (range(shape[0]), range(shape[1]))
-            evaluated = [self.__eval_fun(self.__data[x, y, :])
+            evaluated = [self.__eval_fun(self.__vars['data'][x, y, :])
                          for x, y in itertools.product(*intervals)]
-            length = len(self.__eval_fun(self.__data[0, 0, :]))
-            self.__evaluated = np.array(evaluated).reshape(shape[0], shape[1], length)
-            scipy.io.savemat(file_path, mdict={'data': self.__evaluated}, oned_as='row')
-        print('shape of evaluated data:', np.shape(self.__evaluated))
+            length = len(self.__eval_fun(self.__vars['data'][0, 0, :]))
+            out = np.array(evaluated).reshape(shape[0], shape[1], length)
+            scipy.io.savemat(file_path, mdict={'data': out}, oned_as='row')
+        print('shape of evaluated data:', np.shape(out))
+        return out
 
     def __eval_fun(self, data):
-        tmp = list(zip(self.__targets, data))
+        tmp = list(zip(self.__vars['targets'], data))
         sick = [x[1] for x in tmp if x[0] == 1]
         healthy = [x[1] for x in tmp if x[0] == 0]
         sick_mean = np.mean(sick)
@@ -84,16 +83,16 @@ class FindFeatures():
         """TODO: Docstring for plot_mean_var.
         """
         width = 0.3
-        length = len(self.__evaluated[:, 0, 0])
+        length = len(self.__vars['evaluated'][:, 0, 0])
         path = os.path.join(PLOT_DIRECTORY, 'plot_mean_var_diff.pdf')
         pdf = backend_pdf.PdfPages(path)
         for i in range(length):
-            data = self.__evaluated[i, :, :].transpose()
+            data = self.__vars['evaluated'][i, :, :].transpose()
             x_range = range(len(data[0, :]))
             fig = plt.figure()
             fig.suptitle('bin {}'.format(i), fontsize=12)
             y_range = (np.fabs(data[0, :]-data[2, :])-np.sqrt(
-                np.divide(data[1, :]+data[3, :], self.__var_fac)))
+                np.divide(data[1, :]+data[3, :], self.__vars['fac'])))
             plt.bar(x_range, y_range, width, color='black', linewidth=0)
             pdf.savefig(fig)
             plt.close(fig)
@@ -103,16 +102,16 @@ class FindFeatures():
         """TODO: Docstring for plot_mean_var.
         """
         width = 0.3
-        length = len(self.__evaluated[:, 0, 0])
+        length = len(self.__vars['evaluated'][:, 0, 0])
         path = os.path.join(PLOT_DIRECTORY, 'plot_mean_var.pdf')
         pdf = backend_pdf.PdfPages(path)
         for i in range(length):
-            data = self.__evaluated[i, :, :].transpose()
+            data = self.__vars['evaluated'][i, :, :].transpose()
             x_range = range(len(data[0, :]))
             fig = plt.figure()
             fig.suptitle('bin {}'.format(i), fontsize=12)
             plt.bar(x_range, np.fabs(data[0, :]-data[2, :]), width, color='g', linewidth=0)
-            plt.bar(x_range, np.sqrt(np.divide(data[1, :]+data[3, :], self.__var_fac)),
+            plt.bar(x_range, np.sqrt(np.divide(data[1, :]+data[3, :], self.__vars['fac'])),
                     width, color='r', linewidth=0)
             pdf.savefig(fig)
             plt.close(fig)
@@ -121,11 +120,11 @@ class FindFeatures():
     def plot_mean(self):
         """TODO: Docstring for plot_mean.
         """
-        length = len(self.__evaluated[:, 0, 0])
+        length = len(self.__vars['evaluated'][:, 0, 0])
         path = os.path.join(PLOT_DIRECTORY, "plot_mean.pdf")
         pdf = backend_pdf.PdfPages(path)
         for i in range(length):
-            data = self.__evaluated[i, :, :].transpose()
+            data = self.__vars['evaluated'][i, :, :].transpose()
             x_range = range(len(data[0, :]))
             fig = plt.figure()
             fig.suptitle('bin {}'.format(i), fontsize=12)
@@ -137,11 +136,11 @@ class FindFeatures():
     def plot_var(self):
         """TODO: Docstring for plot_var.
         """
-        length = len(self.__evaluated[:, 0, 0])
+        length = len(self.__vars['evaluated'][:, 0, 0])
         path = os.path.join(PLOT_DIRECTORY, "plot_var.pdf")
         pdf = backend_pdf.PdfPages(path)
         for i in range(length):
-            data = self.__evaluated[i, :, :].transpose()
+            data = self.__vars['evaluated'][i, :, :].transpose()
             x_range = range(len(data[0, :]))
             fig = plt.figure()
             plt.scatter(x_range, np.sqrt(np.fabs(data[1, :])),
@@ -155,8 +154,8 @@ class FindFeatures():
     def plot_significant(self):
         """ plot_significant
         """
-        locations = self.__locations
-        mean_dif = np.fabs(self.__evaluated[:, :, 0]-self.__evaluated[:, :, 2])
+        locations = self.__vars['locations']
+        mean_dif = np.fabs(self.__vars['evaluated'][:, :, 0]-self.__vars['evaluated'][:, :, 2])
         z_range = range(np.shape(locations)[0])
         z_val = np.array([mean_dif[locations[i, 0], locations[i, 1]] for i in z_range])
 
